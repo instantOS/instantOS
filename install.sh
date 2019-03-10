46a64475b2d4c200bd140c1e7dcf020a39bd709a
Paper Benni <paperbenni@gmail.com>
2019-03-10 23:21:50 +0100

    monaco font

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@ gclone() {
     git clone --depth=1 https://github.com/paperbenni/"$1".git
 }
 
+mkdir -p ~/.local/share/fonts
+
+pushd ~/.local/share/fonts
+if ! [ -e monaco.ttf ]; then
+    wget https://github.com/todylu/monaco.ttf/raw/master/monaco.ttf
+fi
+popd
+
 rm -rf ~/suckless
 mkdir ~/suckless
 cd ~/suckless
@@ -12,7 +20,7 @@ gclone dmenu
 gclone slock
 gclone st
 wget https://raw.githubusercontent.com/paperbenni/suckless/master/dwm.desktop
-sudo mv dwm.desktop  /usr/share/xsessions/
+sudo mv dwm.desktop /usr/share/xsessions/
 
 for FOLDER in ./*; do
     if ! [ -d "$FOLDER" ]; then
