03b58fdc16533fec360583dde4c7ffc0d5ac9970
paperbenni <paperbenni@gmail.com>
2019-09-03 15:28:03 +0200

    startdwm

--- install.sh
+++ install.sh
@@ -29,6 +29,9 @@ gclone st
 gclone slock
 
 wget https://raw.githubusercontent.com/paperbenni/suckless/master/dwm.desktop
+wget https://raw.githubusercontent.com/paperbenni/suckless/master/startdwm
+sudo mv startdwm bin/
+sudo chmod +x bin/startdwm
 sudo mv dwm.desktop /usr/share/xsessions/
 
 for FOLDER in ./*; do
