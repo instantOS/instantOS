9518ff31a7ab6d6e6d2870cf8d46e7d0210167e6
paperbenni <paperbenni@gmail.com>
2019-11-03 23:06:36 +0100

    fix autostart instance detection

--- autostart.sh
+++ autostart.sh
@@ -4,32 +4,28 @@
 ## script for paperbenni-dwm autostart            ##
 ####################################################
 
-AUTOSTARTID="$(pgrep autostart.sh)"
-n="${AUTOSTARTID//[^\n]/}"
-if [ ${#n} -eq 1 ]; then
-	echo "No running instances found"
-else
-	echo "another instance already running, exiting"
-	exit
-fi
+for i in "$(pgrep autostart.sh)"; do
+	if [ -z "$AUTOSTARTPID" ]; then
+		AUTOSTARTPID="$i"
+	else
+		echo "other instance of dwm autostart already running"
+		exit
+	fi
+done
 
 if command -v mpv && [ -e ~/paperbenni/boot.wav ]; then
 	mpv ~/paperbenni/boot.wav &
 fi &
 
-if ! [ -e ~/.dwmrunning ]; then
-	while :; do
-		date="$(date)"
-		ping -q -c 1 -W 1 8.8.8.8 && date="$date|""🌍"
-		date="$date|🔊$(amixer get Master | egrep -o '[0-9]{1,3}%' | head -1)"
-		xsetroot -name "$date"
-		sleep 1m
+while :; do
+	date="$(date)"
+	ping -q -c 1 -W 1 8.8.8.8 && date="$date|""🌍"
+	date="$date|🔊$(amixer get Master | egrep -o '[0-9]{1,3}%' | head -1)"
+	xsetroot -name "$date"
+	sleep 1m
 
-	done &
-	compton &
-fi
-
-touch ~/.dwmrunning
+done &
+compton &
 
 if ! pgrep mate-settings; then
 	while :; do
