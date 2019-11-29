9834f16623c59c56b2d27489ba99e27143a22450
paperbenni <paperbenni@gmail.com>
2019-11-29 10:01:07 +0100

    status bar loop without quitting

--- autostart.sh
+++ autostart.sh
@@ -15,32 +15,6 @@ for i in $(pidof -x autostart.sh); do
 done
 
 [ -e ~/.cache/islaptop ] && ISLAPTOP="true"
-INTERNET="X"
-
-REPETITIONS="xxxxxx"
-
-# status bar loop
-while :; do
-	# run every 60 seconds
-	if [ "$REPETITIONS" = "xxxxxx" ]; then
-		if ping -q -c 1 -W 1 8.8.8.8; then
-			INTERNET="🌍"
-		else
-			INTERNET="X"
-		fi
-		REPETITIONS="x"
-	else
-		REPETITIONS="$REPETITIONS"x
-	fi
-
-	date="$(date +'%d-%m-%Y|%T')"
-
-	# battery indicator on laptop
-	[ -n "$ISLAPTOP" ] && date="$date|$(acpi | egrep -o '[0-9]*%')"
-	date="$date|🔊$(amixer get Master | egrep -o '[0-9]{1,3}%' | head -1)|$INTERNET"
-	xsetroot -name "$date"
-	sleep 10
-done &
 
 if command -v picom &>/dev/null; then
 	picom &
@@ -68,3 +42,30 @@ fi
 setxkbmap -layout de
 
 [ -n "$ISLAPTOP" ] && nm-applet &
+
+INTERNET="X"
+
+REPETITIONS="xxxxxx"
+
+# status bar loop
+while :; do
+	# run every 60 seconds
+	if [ "$REPETITIONS" = "xxxxxx" ]; then
+		if ping -q -c 1 -W 1 8.8.8.8; then
+			INTERNET="🌍"
+		else
+			INTERNET="X"
+		fi
+		REPETITIONS="x"
+	else
+		REPETITIONS="$REPETITIONS"x
+	fi
+
+	date="$(date +'%d-%m-%Y|%T')"
+
+	# battery indicator on laptop
+	[ -n "$ISLAPTOP" ] && date="$date|$(acpi | egrep -o '[0-9]*%')"
+	date="$date|🔊$(amixer get Master | egrep -o '[0-9]{1,3}%' | head -1)|$INTERNET"
+	xsetroot -name "$date"
+	sleep 10
+done
