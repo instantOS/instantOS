b20f243fbaaf64a898deb04cb97039a8655b3fc6
paperbenni <paperbenni@gmail.com>
2019-12-31 14:46:47 +0100

    manjaro cursors and fonts

--- themes/manjaro.sh
+++ themes/manjaro.sh
@@ -15,6 +15,7 @@ if ! themeexists matcha; then
     cd ..
     rm -rf matcha
 fi
+gtktheme "Matcha-sea"
 
 if ! icons_exist "Papirus-Maia"; then
     git clone --depth=1 https://github.com/Ste74/papirus-maia-icon-theme.git
@@ -23,5 +24,21 @@ if ! icons_exist "Papirus-Maia"; then
     mv Papirus* ~/.icons
     cd ..
     rm -rf papirus-maia-icon-theme
-
 fi
+
+gtkicons "Papirus-Maia"
+
+# rofi setup
+mkdir -p ~/.config/rofi &>/dev/null
+curl -s "https://raw.githubusercontent.com/paperbenni/dotfiles/master/rofi/manjaro.rasi" >~/.config/rofi/arc.rasi
+echo 'rofi.theme: ~/.config/rofi/manjaro.rasi' >~/.config/rofi/config
+
+curl -s "https://raw.githubusercontent.com/paperbenni/dotfiles/master/fonts/sourcecodepro.sh" | bash
+curl -s "https://raw.githubusercontent.com/paperbenni/dotfiles/master/fonts/roboto.sh" | bash
+
+if ! icons_exist "Breeze"; then
+    mkdir ~/.icons &>/dev/null && cd ~/.icons
+    svn export "https://github.com/KDE/breeze.git/trunk/cursors/Breeze/Breeze"
+fi
+
+echo "done installing manjaro theme"
