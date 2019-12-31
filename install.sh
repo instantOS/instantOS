2324bb7d2c0bb547deadfc7c85e4c9878d3d40f6
paperbenni <paperbenni@gmail.com>
2019-12-31 14:46:30 +0100

    reorder package installations

--- install.sh
+++ install.sh
@@ -11,6 +11,112 @@ source <(curl -s https://raw.githubusercontent.com/paperbenni/bash/master/import
 pb install
 pb git
 
+LINK="https://raw.githubusercontent.com/paperbenni/suckless/master"
+
+cat /etc/os-release | grep -Eiq 'name.*(arch|manjaro|ubuntu)' || exit
+
+if cat /etc/os-release | grep -iq 'name.*arch' ||
+    cat /etc/os-release | grep -iq 'name.*manjaro'; then
+    pacinstall() {
+        for i in "$@"; do
+            { pacman -iQ "$i" || command -v "$i" &>/dev/null; } &>/dev/null && continue
+            sudo pacman -S --noconfirm "$i"
+        done
+    }
+    echo "setting up arch specific stuff"
+
+    sudo pacman -Syu --noconfirm
+
+    # utilities
+    pacinstall picom
+    pacinstall rofi
+    pacinstall dunst
+
+    pacinstall bash dash tmux
+    pacinstall dialog
+    pacinstall wget slop
+    pacinstall acpi
+    pacinstall cpio
+
+    aptinstall git
+    aptinstall subversion
+
+    pacinstall ffmpeg
+    pacinstall feh
+    pacinstall mpv
+
+    pacinstall wmctrl
+    pacinstall xdotool
+    pacinstall xrandr
+    pacinstall xorg-xsetroot
+
+    pacinstall conky
+    pacinstall ranger
+    pacinstall fzf
+    pacinstall sl
+
+    pacinstall xorg-fonts-misc
+    pacinstall lxappearance
+
+    if ! command -v panther_launcher; then
+        wget "https://www.rastersoft.com/descargas/panther_launcher/panther_launcher-1.12.0-1-x86_64.pkg.tar.xz"
+        sudo pacman -U --noconfirm panther_launcher*.pkg.tar.xz
+        rm panther_launcher*.pkg.tar.xz
+    fi
+
+fi
+
+# ubuntu specific stuff
+if grep -iq 'name.*ubuntu' </etc/os-release; then
+
+    sudo apt-get update
+    sudo apt-get upgrade -y
+
+    aptinstall() {
+        for i in "$@"; do
+            { dpkg -l "$i" || command -v "$i" &>/dev/null; } &>/dev/null && continue
+            sudo apt-get install -y "$i"
+        done
+    }
+
+    aptinstall compton
+
+    aptinstall git
+    aptinstall subversion
+
+    aptinstall bash dash tmux
+    aptinstall dialog
+    aptinstall wget
+
+    aptinstall slop
+    aptinstall rofi
+    aptinstall dunst
+
+    aptinstall acpi
+    aptinstall xrandr
+    aptinstall x11-xserver-utils
+
+    aptinstall ffmpeg
+    aptinstall feh
+    aptinstall mpv
+
+    aptinstall cpio
+
+    aptinstall fzf
+    aptinstall ranger
+    aptinstall conky
+    aptinstall sl
+
+    aptinstall lxappearance
+
+    if ! command -v panther_launcher; then
+        wget "https://www.rastersoft.com/descargas/panther_launcher/panther-launcher-xenial_1.12.0-ubuntu1_amd64.deb"
+        sudo dpkg -i panther-launcher*.deb
+        sudo apt-get install -fy
+        rm panther-launcher*.deb
+    fi
+fi
+
 gprogram() {
     wget "https://raw.githubusercontent.com/paperbenni/suckless/master/programs/$1"
     usrbin -f "$1"
@@ -107,56 +213,6 @@ if [ -n "$DOTFILES" ]; then
     curl https://raw.githubusercontent.com/paperbenni/dotfiles/master/install.sh | bash
 fi
 
-LINK="https://raw.githubusercontent.com/paperbenni/suckless/master"
-
-if cat /etc/os-release | grep -iq 'name.*arch' ||
-    cat /etc/os-release | grep -iq 'name.*manjaro'; then
-    pacinstall() {
-        for i in "$@"; do
-            { pacman -iQ "$i" || command -v "$i"; } &>/dev/null && continue
-            sudo pacman -S --noconfirm "$i"
-        done
-    }
-    echo "setting up arch specific stuff"
-
-    sudo pacman -Syu --noconfirm
-
-    # utilities
-    pacinstall picom
-    pacinstall rofi
-    pacinstall dunst
-
-    pacinstall bash dash tmux
-    pacinstall dialog
-    pacinstall wget slop
-    pacinstall acpi
-    pacinstall cpio
-
-    pacinstall ffmpeg
-    pacinstall feh
-    pacinstall mpv
-
-    pacinstall wmctrl
-    pacinstall xdotool
-    pacinstall xrandr
-    pacinstall xorg-xsetroot
-
-    pacinstall conky
-    pacinstall ranger
-    pacinstall fzf
-    pacinstall sl
-
-    pacinstall xorg-fonts-misc
-    pacinstall lxappearance
-
-    if ! command -v panther_launcher; then
-        wget "https://www.rastersoft.com/descargas/panther_launcher/panther_launcher-1.12.0-1-x86_64.pkg.tar.xz"
-        sudo pacman -U --noconfirm panther_launcher*.pkg.tar.xz
-        rm panther_launcher*.pkg.tar.xz
-    fi
-
-fi
-
 if ! [ ~/.local/share/fonts/symbola.ttf ]; then
     mkdir -p ~/.local/share/fonts
     cd ~/.local/share/fonts
@@ -164,54 +220,6 @@ if ! [ ~/.local/share/fonts/symbola.ttf ]; then
 fi
 
 cd
-
-# ubuntu specific stuff
-if grep -iq 'name.*ubuntu' </etc/os-release; then
-
-    sudo apt-get update
-    sudo apt-get upgrade -y
-
-    aptinstall() {
-        for i in "$@"; do
-            { dpkg -l "$i" || command -v "$i"; } &>/dev/null && continue
-            sudo apt-get install -y "$i"
-        done
-    }
-
-    aptinstall compton
-
-    aptinstall bash dash tmux
-    aptinstall dialog
-    aptinstall wget
-
-    aptinstall slop
-    aptinstall rofi
-    aptinstall dunst
-
-    aptinstall acpi
-    aptinstall xrandr
-    aptinstall x11-xserver-utils
-
-    aptinstall ffmpeg
-    aptinstall feh
-    aptinstall mpv
-
-    aptinstall cpio
-
-    aptinstall fzf
-    aptinstall ranger
-    aptinstall conky
-    aptinstall sl
-
-    aptinstall lxappearance
-
-    if ! command -v panther_launcher; then
-        wget "https://www.rastersoft.com/descargas/panther_launcher/panther-launcher-xenial_1.12.0-ubuntu1_amd64.deb"
-        sudo dpkg -i panther-launcher*.deb
-        sudo apt-get install -fy
-        rm panther-launcher*.deb
-    fi
-fi
 
 # laptop specific stuff
 if acpi | grep -q '[0-9]%'; then
