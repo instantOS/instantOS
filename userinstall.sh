1cfa0dc470de8e65549cd4cc37406b07d7c39b01
paperbenni <paperbenni@gmail.com>
2020-02-01 16:18:46 +0100

    move notif sound fetching to dunsttrigger

--- userinstall.sh
+++ userinstall.sh
@@ -9,18 +9,7 @@ cd "$HOME"
 echo "installing instantOS tools"
 
 RAW="https://raw.githubusercontent.com"
-source <(curl -s $RAW/paperbenni/bash/master/import.sh)
-pb install
-pb git
-
 LINK="$RAW/instantos/instantos/master"
-
-if ! [ ~/.local/share/fonts/symbola.ttf ]; then
-    mkdir -p .local/share/fonts
-    cd .local/share/fonts
-    echo "installing symbola font"
-    wget -q "http://symbola.surge.sh/symbola.ttf"
-fi
 
 cd "$HOME"
 
@@ -32,13 +21,8 @@ fi
 
 cd
 mkdir -p instantos/notifications &>/dev/null
-cd instantos/notifications
 
-if ! [ -e notification.ogg ]; then
-    wget -qO notification.ogg "https://notificationsounds.com/notification-sounds/me-too-603/download/ogg"
-fi
-
-cd ~/instantos
+cd instantos
 rm -rf wallpapers
 mkdir wallpapers
 
