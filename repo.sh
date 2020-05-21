f563f6421591e7b250f8256cefc8d9b64980eebc
paperbenni <paperbenni@gmail.com>
2020-05-21 19:15:08 +0200

    add 32 bit repo detection

--- repo.sh
+++ repo.sh
@@ -2,8 +2,18 @@
 
 echo "adding instantOS repo"
 
-if ! grep -q 'instantos\.surge\.sh' /etc/pacman.conf; then
-    echo "[instant]" >>/etc/pacman.conf
-    echo "SigLevel = Optional TrustAll" >>/etc/pacman.conf
-    echo "Server = http://instantos.surge.sh" >>/etc/pacman.conf
+if ! uname -m | grep -q '^i'; then
+    # default is 64 bit repo
+    if ! grep -q 'instantos\.surge\.sh' /etc/pacman.conf; then
+        echo "[instant]" >>/etc/pacman.conf
+        echo "SigLevel = Optional TrustAll" >>/etc/pacman.conf
+        echo "Server = http://instantos.surge.sh" >>/etc/pacman.conf
+    fi
+else
+    # 32 bit has a seperate repo (obviously)
+    if ! grep -q 'instantos\.surge\.sh' /etc/pacman.conf; then
+        echo "[instant]" >>/etc/pacman.conf
+        echo "SigLevel = Optional TrustAll" >>/etc/pacman.conf
+        echo "Server = http://instantos32.surge.sh" >>/etc/pacman.conf
+    fi
 fi
