07bd9b5349b425be9f487272c75b1ed03de38e20
paperbenni <paperbenni@gmail.com>
2020-08-02 16:49:13 +0200

    add alttab to instantutils

--- instantutils.sh
+++ instantutils.sh
@@ -6,6 +6,7 @@ USAGE="usage: instantutils install
 instantutils root
 instantutils user
 instantutils repo
+instantutils alttab
 instantutils refresh"
 
 if [ -z "$1" ]; then
@@ -22,6 +23,9 @@ root)
     ;;
 default)
     /usr/share/instantutils/setup/defaultapps
+    ;;
+alttab)
+    alttab -fg "#ffffff" -bg "#292F3A" -frame "#5293E1" -d 0 -s 1 -t 128x150 -i 127x64 -w 1 -vp pointer &
     ;;
 user)
     /usr/share/instantutils/userinstall.sh
