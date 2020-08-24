dafa04ff5ba574694eaec13747487e58fac4c2e8
paperbenni <paperbenni@gmail.com>
2020-08-24 19:46:47 +0200

    new repo.sh script

--- repo.sh
+++ repo.sh
@@ -4,21 +4,30 @@
 ## add repo containing instantOS programs and required prebuilt aur programs ##
 ###############################################################################
 
-echo "adding instantOS repo"
+
+whoami | grep -q 'root' || { echo "please run this as root" && exit 1
+}
+
+echo "adding instantOS repos"
 
 addrepo() {
-    if ! grep -q '\[instant\]' /etc/pacman.conf; then
-        echo "adding $1 repo"
-        echo "[instant]" >>/etc/pacman.conf
-        echo "SigLevel = Optional TrustAll" >>/etc/pacman.conf
-        echo "Include = /etc/pacman.d/instantmirrorlist" >>/etc/pacman.conf
-        if [ -e /usr/share/instantutils/mirrors/"$1" ]; then
-            cat /usr/share/instantutils/mirrors/"$1" >/etc/pacman.d/instantmirrorlist
-        else
-            curl -s https://raw.githubusercontent.com/instantOS/instantOS/master/mirrors/"$1" >/etc/pacman.d/instantmirrorlist
-        fi
+
+    if grep -q '\[instant\]' /etc/pacman.conf; then
+        echo "removing old mirrors"
+        sed -i '/^\[instant\]/,+2d' /etc/pacman.conf
+    fi
+
+    echo "adding $1 repo"
+    {
+        echo "[instant]"
+        echo "SigLevel = Optional TrustAll"
+        echo "Include = /etc/pacman.d/instantmirrorlist"
+    }>>/etc/pacman.conf
+
+    if [ -e /usr/share/instantutils/mirrors/"$1" ]; then
+        cat /usr/share/instantutils/mirrors/"$1" >/etc/pacman.d/instantmirrorlist
     else
-        echo "instantOS $1 repository already added"
+        curl -s https://raw.githubusercontent.com/instantOS/instantOS/master/mirrors/"$1" >/etc/pacman.d/instantmirrorlist
     fi
 
 }
@@ -37,3 +46,6 @@ elif uname -m | grep '^i'; then
 else
     echo "no suitable repo for architecture found"
 fi
+
+echo "done adding the instantos repository"
+
